b8f5d7f8a4cb23f355d688d4bbb871485c688222
omargherbawi <omarahmad12318@gmail.com>
2025-06-23 00:20:16 +0300

    list view zoom

--- lib/Features/Home/presention/views/widget/featured_list_view.dart
+++ lib/Features/Home/presention/views/widget/featured_list_view.dart
@@ -1,20 +1,56 @@
-import 'package:book_app/Features/Home/presention/views/widget/featured_list_view_item.dart';
 import 'package:flutter/material.dart';
 
-class FeaturedListView extends StatelessWidget {
+import 'featured_list_view_item.dart';
+
+class FeaturedListView extends StatefulWidget {
   const FeaturedListView({super.key});
+
+  @override
+  State<FeaturedListView> createState() => _FeaturedListViewState();
+}
+
+class _FeaturedListViewState extends State<FeaturedListView> {
+  final ScrollController _controller = ScrollController();
+  int activeIndex = 0;
+  double itemWidth = 150;
+
+  void _onScroll() {
+    double offset = _controller.offset;
+    int newIndex = (offset / (itemWidth + 12)).round();
+
+    if (newIndex != activeIndex) {
+      setState(() {
+        activeIndex = newIndex;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(_onScroll);
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(_onScroll);
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: MediaQuery.of(context).size.height * 0.3,
       child: ListView.builder(
+        controller: _controller,
         scrollDirection: Axis.horizontal,
         itemCount: 10,
         itemBuilder: (context, index) {
+          bool isActive = index == activeIndex;
           return Padding(
             padding: const EdgeInsets.all(6),
-            child: const FeaturedListViewitem(),
+            child: FeaturedListViewitem(isActive: isActive),
           );
         },
       ),
--- lib/Features/Home/presention/views/widget/featured_list_view_item.dart
+++ lib/Features/Home/presention/views/widget/featured_list_view_item.dart
@@ -2,20 +2,29 @@ import 'package:book_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
 class FeaturedListViewitem extends StatelessWidget {
-  const FeaturedListViewitem({super.key});
+  final bool isActive;
+
+  const FeaturedListViewitem({super.key, this.isActive = false});
 
   @override
   Widget build(BuildContext context) {
+    double scale = isActive ? 1.1 : 1.0;
+
     return SizedBox(
       height: MediaQuery.of(context).size.height * 0.3,
       child: AspectRatio(
         aspectRatio: 2.6 / 4,
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(35),
-            image: DecorationImage(
-              image: AssetImage(AssetsData.book),
-              fit: BoxFit.fill,
+        child: AnimatedScale(
+          scale: scale,
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeInOut,
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(35),
+              image: DecorationImage(
+                image: AssetImage(AssetsData.book),
+                fit: BoxFit.fill,
+              ),
             ),
           ),
         ),
