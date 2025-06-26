e9e9b47ab08cbc3cd3f5183f37be8c44ce2a3ac6
omargherbawi <omarahmad12318@gmail.com>
2025-06-26 18:27:46 +0300

    errors handling

--- lib/Features/Home/data/repos/home_repo_impl.dart
+++ lib/Features/Home/data/repos/home_repo_impl.dart
@@ -3,6 +3,7 @@ import 'package:book_app/Features/Home/data/repos/home_repo.dart';
 import 'package:book_app/core/errors/failure.dart';
 import 'package:book_app/core/utils/api_service.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
   final ApiService apiService;
@@ -11,34 +12,50 @@ class HomeRepoImpl implements HomeRepo {
 
   @override
   Future<Either<Failure, List<BookModel>>> fetchnewestbook() async {
-    var data = await apiService.get(
-      endpoint:
-          'volumes?Filtering=free-ebooks&Sorting=newest&q=subject:programming',
-    );
+    try {
+      var data = await apiService.get(
+        endpoint:
+            'volumes?Filtering=free-ebooks&Sorting=newest&q=subject:programming',
+      );
 
-    List<BookModel> books = [];
+      List<BookModel> books = [];
 
-    for (var item in data['items']) {
-      books.add(BookModel.fromJson(item));
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      } else {
+        return left(ServerFailure(e.toString()));
+      }
     }
-
-    return right(books);
   }
 
   @override
   Future<Either<Failure, List<BookModel>>> fetchfeaturedbook() async {
-    final data = await apiService.get(
-      endpoint: 'volumes?Filtering=free-ebooks&q=subject:programming',
-    );
+    try {
+      final data = await apiService.get(
+        endpoint: 'volumes?Filtering=free-ebooks&q=subject:programming',
+      );
 
-    List<BookModel> books = [];
+      List<BookModel> books = [];
 
-    if (data['items'] != null) {
-      for (var item in data['items']) {
-        books.add(BookModel.fromJson(item));
+      if (data['items'] != null) {
+        for (var item in data['items']) {
+          books.add(BookModel.fromJson(item));
+        }
+      }
+
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      } else {
+        return left(ServerFailure(e.toString()));
       }
     }
-
-    return right(books);
   }
 }
--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,3 +1,52 @@
+import 'package:dio/dio.dart';
+
 abstract class Failure {}
 
-class ServerFailure extends Failure {}
+class ServerFailure extends Failure {
+  final String errmessage;
+
+  ServerFailure(this.errmessage);
+
+  factory ServerFailure.fromDioError(DioException dioError) {
+    switch (dioError.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('connect Time Out');
+
+      case DioExceptionType.sendTimeout:
+        return ServerFailure(' Send Time Out');
+
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Receive Time Out');
+
+      case DioExceptionType.badCertificate:
+        return ServerFailure('Bad Certificate');
+
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(
+          dioError.response!.statusCode!,
+          dioError.response!.data,
+        );
+
+      case DioExceptionType.cancel:
+        return ServerFailure('Request Canceled');
+
+      case DioExceptionType.connectionError:
+        return ServerFailure('Connection Error');
+
+      case DioExceptionType.unknown:
+        return ServerFailure('Unknown Error');
+    }
+  }
+
+  factory ServerFailure.fromResponse(int statusCode, dynamic response) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(response['error']['message']);
+    } else if (statusCode == 404) {
+      return ServerFailure('Page Not Found');
+    } else if (statusCode == 500) {
+      return ServerFailure('Internal Server Error');
+    } else {
+      return ServerFailure(response['error']['message']);
+    }
+  }
+}
