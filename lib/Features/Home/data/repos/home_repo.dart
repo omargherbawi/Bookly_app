472a1bd2ef2762522e00a50f191f65797b88fb56
omargherbawi <omarahmad12318@gmail.com>
2025-06-26 17:30:20 +0300

    home repo done

---
+++ lib/Features/Home/data/repos/home_repo.dart
@@ -0,0 +1,9 @@
+import 'package:book_app/Features/Home/data/models/book_model/book_model.dart';
+import 'package:book_app/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure, List<BookModel>>> fetchnewestbook();
+
+  Future<Either<Failure, List<BookModel>>> fetchfeaturedbook();
+}
